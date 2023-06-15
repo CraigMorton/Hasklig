7158ac55347c6e42601dee4f5ce46580ac55fa5d
Craig Morton <CraigMorton22@gmail.com>
2023-06-15 13:41:29 +0100

    Use absolute path to script in makeInstances.sh as in build.sh
    The two main commands in `README.md` that will be run by someone who wants to install Hasklig (on linux / macOS) are `makeInstances.sh` and `build.sh`.
    
    Currently `build.sh` uses absolute paths determined by the path of the script itself, so it can be run from anywhere. `makeInstances.sh` does not, this adds the absolute path behaviour to `makeInstances.sh`
    
    Perhaps this will cause `makeInstances.sh` to fail on Windows. I haven't tested on Windows myself, sorry. Since Windows already has a separate build script, it could have a separate makeInstances script as well. That might not be worth the hassle, but it also could clarify the two separate install process for Windows and Linux/macOS

--- makeInstances.sh
+++ makeInstances.sh
@@ -1,2 +1,5 @@
-makeInstancesUFO -d Roman/Masters/SourceCodePro.designspace
-makeInstancesUFO -d Italic/Masters/SourceCodePro-Italic.designspace
+# get absolute path to bash script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+makeInstancesUFO -d $DIR/Roman/Masters/SourceCodePro.designspace
+makeInstancesUFO -d $DIR/Italic/Masters/SourceCodePro-Italic.designspace
